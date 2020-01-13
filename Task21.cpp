b1870dd17af7d1a55fd911e0eab018e6fbf7468c
GachiGucciGhoul <55503682+GachiGucciGhoul@users.noreply.github.com>
2020-01-13 22:20:39 +0300

    Add files via upload

---
+++ Task21.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <ctime>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	srand(time(NULL));
+	const int size1 = 10, size2 = 8;
+	int arr[size1][size2]{ {} };
+	int i, j, k=0, iTry, jTry;
+	i = rand() % 10;		
+	j = rand() % 8;
+	arr[i][j] = 6;
+
+	while (k!=5)
+	{
+		std::cout << "Введите координаты: ";
+		std::cin >> iTry >> jTry;
+
+		if ((iTry > size1-1) || (jTry > size2-1) || (iTry < 0) || (jTry < 0))
+		{
+			std::cout << "Выход за границу массива\n";
+			continue;
+		}
+
+		if (arr[iTry][jTry] == 1)
+		{
+			std::cout << "Эту уже проверяли\n";
+			continue;
+		}
+
+		if (arr[iTry][jTry] == 0)
+		{
+			arr[iTry][jTry] = 1;
+			k++;
+			std::cout << "Нет\n";
+		}
+
+		if (arr[iTry][jTry] == 6)
+		{
+			std::cout << "Вы угадали\n";
+			return 0;
+		}
+
+	}
+
+	std::cout << "GAME OVER\n"<<i<<"\t"<<j;
+	
+}
---
+++ Task23.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+int CharToInt(char R)
+{
+	int r = (int)R;
+	if ((r >= 65) && (r <= 90))
+		r += 32;
+	if (((r >= 97) && (r <= 122)) || ((r >= 48) && (r <= 57)))
+		return r;
+	else return 1;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	bool check = true;
+	int size2, l, r, a, b;
+	const short SIZE = 21;
+	char str[SIZE];
+
+	std::cin.getline(str, '\n');
+	size2 = strlen(str);
+
+	l = 0;
+	r = size2 - 1;
+	
+
+	while (l < r)
+	{
+		a = 1;
+		b = 1;
+		while (a == 1)
+		{
+			a = CharToInt(str[l]);
+			if (a == 1)
+				l++;
+		}
+
+
+		while (b == 1)
+		{
+			b = CharToInt(str[r]);
+			if (b == 1)
+				r--;
+		}
+	
+		if (a != b)
+		{
+			check = false;
+			break;
+		}
+		l++;
+		r--;
+	}
+
+	if (check)
+		std::cout << "Палиндром";
+	else
+		std::cout << "Не палиндром";
+}
